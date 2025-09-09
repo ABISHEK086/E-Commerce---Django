1348f4f00f577652a093715cc2e70bf8a07a2499
ABISHEK ANTONY <abishek8673@gmail.com>
2025-09-09 19:05:00 +0530

    Update urlify.js

--- static/admin/js/urlify.js
+++ static/admin/js/urlify.js
@@ -168,7 +168,7 @@
         } else {
             s = s.replace(/[^-\w\s]/g, ''); 
         }
-        s = s.replace(/^\s+|\s+$/g, ''); // trim leading/trailing spaces
+        s = s.replace(/^\s+|\s+$/g, ''); 
         s = s.replace(/[-\s]+/g, '-'); // convert spaces to hyphens
         s = s.substring(0, num_chars); // trim to first num_chars chars
         s = s.replace(/-+$/g, ''); // trim any trailing hyphens
