7c9f38b23b4067d6903dd0f90db145d3c24ffb19
ABISHEK ANTONY <abishek8673@gmail.com>
2025-11-25 11:20:45 +0530

    Update views.py

--- DjangoEcommerceApp/views.py
+++ DjangoEcommerceApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render     
 from django.http import HttpResponse,HttpResponseRedirect, JsonResponse 
-from django.contrib.auth import authenticate,l ogin,logout, update_session_auth_hash 
+from django.contrib.auth import authenticate,login,logout, update_session_auth_hash 
 from django.contrib import message
 from django.urls import reverse   
 from django.views.decorators.csrf import csrf_exempt  
