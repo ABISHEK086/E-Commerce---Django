b4596c4c6085fd0b8b2cf80b20aaf0fc4697947f
ABISHEK ANTONY <abishek8673@gmail.com>
2025-11-30 21:15:34 +0530

    Update AdminViews.py

--- DjangoEcommerceApp/AdminViews.py
+++ DjangoEcommerceApp/AdminViews.py
@@ -6,7 +6,7 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.core.files.storage import FileSystemStorage  
 from django.contrib.messages.views import messages       
 from django.urls import reverse          
-from django.http import HttpResponseRedirect,HttpResponse    
+from django.http import HttpResponseRedirect,HttpResponse     
 from django.db.models import Q    
 from DjangoEcommerce.settings import BASE_URL    
 from django.views.decorators.csrf import csrf_exempt
