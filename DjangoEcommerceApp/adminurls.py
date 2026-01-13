10cedad704793e376d19136596bb6bfd7bb76809
ABISHEK ANTONY <abishek8673@gmail.com>
2026-01-13 17:54:56 +0530

    Update adminurls.py

--- DjangoEcommerceApp/adminurls.py
+++ DjangoEcommerceApp/adminurls.py
@@ -1,5 +1,5 @@
 from django.urls import path   
-from DjangoEcommerceApp import AdminViews   
+from DjangoEcommerceApp import AdminViews    
 from DjangoEcommerceApp import views                         
                          
 urlpatterns = [            
