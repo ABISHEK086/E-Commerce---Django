5f63d7cf60b87f0238d7ab4c8f093e9eb6b6120f
ABISHEK ANTONY <abishek8673@gmail.com>
2026-02-03 09:09:20 +0530

    Update urls.py

--- DjangoEcommerce/urls.py
+++ DjangoEcommerce/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from django.conf import settings  
 from django.conf.urls.static import static 
 from django.views.generic import TemplateView 
-from DjangoEcommerceApp import views 
+from DjangoEcommerceApp import views  
   
 urlpatterns = [    
     path('admin/', admin.site.urls), 
