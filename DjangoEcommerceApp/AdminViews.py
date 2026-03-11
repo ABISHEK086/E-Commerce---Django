fa4af813e95445a2429ffcd508ea37070e91327e
ABISHEK ANTONY <abishek8673@gmail.com>
2026-03-11 16:03:08 +0530

    Update AdminViews.py

--- DjangoEcommerceApp/AdminViews.py
+++ DjangoEcommerceApp/AdminViews.py
@@ -11,7 +11,7 @@ from django.db.models import Q
 from DjangoEcommerce.settings import BASE_URL                  
 from django.views.decorators.csrf import csrf_exempt  
   
-@login_required(login_url="/admin/")
+@login_required(login_url="/admin/") 
 def admin_home(request):
     return render(request, "home.html")  
 @login_required(login_url="/admin/")
